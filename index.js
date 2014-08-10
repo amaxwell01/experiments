2f185bbe21e8d5244d588cbb1d76d04fbf6494fa
Andrew Maxwell <amaxwell01@gmail.com>
2014-08-09 18:51:25 -0700

    [node_server] Built a new node server to handle the display of my experiments on labs.andrewcmaxwell.com

---
+++ index.js
@@ -0,0 +1,18 @@
+var server = require('http');
+var fs = require('fs');
+var directoryFiles = fs.readdirSync( __dirname );
+var files = '<ul>';
+
+server.createServer( function( request, response ) {
+    response.writeHeader(200, {'Content-Type': 'text/html'});
+    
+    for ( var file in directoryFiles ) {
+        files += '<li><a href="/' + directoryFiles[file] + '" target="_blank">' + directoryFiles[file] + '</a></li>';
+    }
+
+    files += '</ul>';
+
+    response.write( files );
+}).listen(8000);
+
+console.log('Server running at http://127.0.0.1:8000/');
