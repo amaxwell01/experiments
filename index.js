a631eb0c5354c38ac159655aa4536ec04c580b26
Andrew Maxwell <amaxwell01@gmail.com>
2014-09-01 19:06:52 -0700

    [labs] Added better error logging

--- index.js
+++ index.js
@@ -17,44 +17,60 @@ app.get('/*', function(request, response){
         console.log(request.url + ' requested');
         return;
     }
-    
-    if (fs.lstatSync(__dirname + path).isFile()) {
-        response.writeHeader(200, {'Content-Type': 'text/plain'});
-        fs.readFile(__dirname + path, 'utf8', function (error, data) {
-            if (error) {
-                throw error;
-            }
-            response.write(data);
-            response.end();
-        });
-    } else {
-        directoryFiles = fs.readdirSync(__dirname + path);
-        response.writeHeader(200, {'Content-Type': 'text/html'});
-        
-        directoryData += '<html>';
-        directoryData += '<head>';
-        directoryData += '<style>ul {list-style-position: inside; padding-left: 0;} li {padding: 5px; color: red; border: solid 1px #CCC; list-style-type: none;} li a {color: #333; text-decoration: none;}</style>';
-        directoryData += '</head>';
-        directoryData += '<body>';
-        directoryData += '<ul>';
-        directoryData += '<li><a href="../" target="_self"/>..</a></li>';
 
-        for (file in directoryFiles) {
-            if (fs.lstatSync(__dirname + path + directoryFiles[file]).isDirectory()) {
-                folders += '<li><a href="' + path + directoryFiles[file] + '/" target="_self">' + directoryFiles[file] + '</a></li>';
-            } else {
-                files += '<li><a href="' + path + directoryFiles[file] + '" target="_self">' + directoryFiles[file] + '</a></li>';
+    try {
+        if (fs.lstatSync(__dirname + path).isFile()) {
+            response.writeHeader(200, {'Content-Type': 'text/plain'});
+            fs.readFile(__dirname + path, 'utf8', function (error, data) {
+                if (error) {
+                    throw error;
+                }
+                response.write(data);
+                response.end();
+            });
+        } else {
+            directoryFiles = fs.readdirSync(__dirname + path);
+            response.writeHeader(200, {'Content-Type': 'text/html'});
+            
+            directoryData += '<html>';
+            directoryData += '<head>';
+            directoryData += '<style>ul {list-style-position: inside; padding-left: 0;} li {padding: 5px; color: red; border: solid 1px #CCC; list-style-type: none;} li a {color: #333; text-decoration: none;}</style>';
+            directoryData += '</head>';
+            directoryData += '<body>';
+            directoryData += '<ul>';
+            directoryData += '<li><a href="../" target="_self"/>..</a></li>';
+
+            for (file in directoryFiles) {
+                if (fs.lstatSync(__dirname + path + directoryFiles[file]).isDirectory()) {
+                    folders += '<li><a href="' + path + directoryFiles[file] + '/" target="_self">' + directoryFiles[file] + '</a></li>';
+                } else {
+                    files += '<li><a href="' + path + directoryFiles[file] + '" target="_self">' + directoryFiles[file] + '</a></li>';
+                }
             }
+
+            directoryData += folders;
+            directoryData += files;
+
+            directoryData += '</ul>';
+            directoryData += '</body>';
+            directoryData += '</html>';
+            response.write(directoryData);
+            response.end();
+        }
+    } catch (error) {
+        var requestText = ' requested and not found';
+        var data = (__dirname + path) + requestText;
+
+        // If the type is not what you want, then just throw the error again.
+        if (error.code !== 'ENOENT') {
+            throw error;
         }
 
-        directoryData += folders;
-        directoryData += files;
-
-        directoryData += '</ul>';
-        directoryData += '</body>';
-        directoryData += '</html>';
-        response.write(directoryData);
+        // Handle a file-not-found error
+        response.writeHeader(404, {'Content-Type': 'text/plain'});
+        response.write(request.url + requestText);
         response.end();
+        console.log(data);
     }
 });
 
