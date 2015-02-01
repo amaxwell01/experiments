ead277916e21111d55c80ce7d66661b4a1070741
Andrew Maxwell <amaxwell01@gmail.com>
2015-01-31 17:57:36 -0800

    [count-to-6] Finished all of the count-to-6 tutorials

--- javascript/nodejs/nodeschool.io/count-to-6/tagged_template_strings/tagged_template_strings.js
+++ javascript/nodejs/nodeschool.io/count-to-6/tagged_template_strings/tagged_template_strings.js
@@ -1,1 +1,26 @@
 // Tagged template strings
+var a = process.argv[2] || 'l33t haxor';
+var b = process.argv[3] || '<script>alert("pwned!")</script>';
+var html = (strings, ...args) => {
+    var result = '';
+
+    for (var i = 0; i < strings.length; i++) {
+        result += strings[i];
+
+        if (i < args.length) {
+            result += replace(args[i]);
+        }
+    }
+
+    return result;
+};
+
+var replace = function(string) {
+    return string.replace(/&/g, '&amp;')
+                .replace(/'/g, '&#39;')
+                .replace(/"/g, '&quot;')
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;');
+}
+
+console.log(html`<b>${a} says</b>: "${b}"`);
